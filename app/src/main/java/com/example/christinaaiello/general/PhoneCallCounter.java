5885319f1a3d3f1a5364759980ab5b365bb298bd
Christina Aiello <cjaiello@wpi.edu>
2015-04-06 22:45:57 -0400

    updated miscellaneous box padding in all edit modes

--- app/src/main/java/com/example/christinaaiello/general/PhoneCallCounter.java
+++ app/src/main/java/com/example/christinaaiello/general/PhoneCallCounter.java
@@ -10,9 +10,13 @@ import android.provider.CallLog;
 public class PhoneCallCounter {
 
     public static Integer getNumberOfPhoneCalls (Activity activity, String phoneNumber) {
+        // This will replace any dashes, slashes, periods, and/or parentheses with empty space:
         String phoneNumberWithoutDashes = phoneNumber.replaceAll("[\\s\\-().]", "");
+        // This is what we want to search for, the current phone number
         String[] selectionArgs = {String.valueOf(phoneNumberWithoutDashes)};
+        // This is our query
         Cursor cursor = activity.getContentResolver().query(CallLog.Calls.CONTENT_URI, null, CallLog.Calls.NUMBER + " = ? ", selectionArgs, null);
-        return cursor.getCount(); // Returning the number of matches we get with this number
+        // Returning the number of matches we get with this number
+        return cursor.getCount();
     }
 }
